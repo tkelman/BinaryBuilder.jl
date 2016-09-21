e654e1658689a5d5038c3ff2dd7b8d3ecb8c77c9
Tony Kelman <tony@kelman.net>
2016-09-21 01:12:32 -0700

    I'm having too much fun with this

--- setup_proot.sh
+++ setup_proot.sh
@@ -14,3 +14,15 @@ cp ../../usr/include/talloc.h .
 make -j$(nproc) install LDFLAGS="-L../../usr/lib -ltalloc" DESTDIR=../../usr/bin
 cd ../..
 rm -rf PRoot-$PROOT_VER talloc-$TALLOC_VER
+
+# switch to centos6:
+# mkdir centos6
+# curl -L https://github.com/CentOS/sig-cloud-instance-images/raw/CentOS-6/docker/centos-6-docker.tar.xz | tar -C centos6 -xJf -
+# cd centos6
+# ../usr/bin/proot -R .
+
+# switch to opensuse 42.1:
+# mkdir opensuse421
+# curl -L https://github.com/openSUSE/docker-containers-build/raw/openSUSE-42.1/docker/openSUSE-42.1.tar.xz | tar -C opensuse421 -xJf -
+# cd opensuse421
+# ../usr/bin/proot -R .
