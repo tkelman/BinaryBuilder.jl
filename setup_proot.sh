0792ed11f43f3cd2992c920d55b6462da1ed1dc6
Tony Kelman <tony@kelman.net>
2016-09-20 23:13:05 -0700

    build both libtalloc and proot
    a libtalloc with an soname is probably already present but may be annoying to find

--- setup_proot.sh
+++ setup_proot.sh
@@ -2,5 +2,13 @@
 # setup script to bootstrap proot
 set -e # stop on failure
 #cd $(dirname $0)
-curl -L https://github.com/proot-me/PRoot/archive/v5.1.0.tar.gz | tar -xzf -
-curl -L https://www.samba.org/ftp/talloc/talloc-2.1.8.tar.gz | tar -xzf -
+PROOT_VER=5.1.0
+TALLOC_VER=2.1.8
+curl -L https://github.com/proot-me/PRoot/archive/v$PROOT_VER.tar.gz | tar -xzf -
+curl -L https://www.samba.org/ftp/talloc/talloc-$TALLOC_VER.tar.gz | tar -xzf -
+cd talloc-$TALLOC_VER
+./configure --disable-python --prefix=../usr
+make -j$(nproc) install
+cd ../PRoot-$PROOT_VER/src
+cp ../../usr/include/talloc.h .
+make -j$(nproc) install LDFLAGS="-L../../usr/lib -ltalloc" DESTDIR=../../usr/bin
